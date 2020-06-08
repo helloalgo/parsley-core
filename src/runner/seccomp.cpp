0fb286770ffdbda928208bd579791bbbe9e68595
Junghoon Noh <jungnoh.dev@gmail.com>
2020-06-08 20:18:21 +0900

    change seccomp filter to use int[] instead of vector

--- src/runner/seccomp.cpp
+++ src/runner/seccomp.cpp
@@ -3,45 +3,24 @@
 #include <vector>
 #include "runner.hpp"
 
-void allowCalls(scmp_filter_ctx ctx, std::vector<int> calls) {
-  for (auto it=calls.begin(); it!=calls.end(); it++) {
-    seccomp_rule_add(ctx, SCMP_ACT_ALLOW, *it, 0);
-  }
+void allowCalls(scmp_filter_ctx ctx, int calls[]) {
+    int length = sizeof(calls) / sizeof(int);
+    for (int i=0;i<length;i++) {
+        seccomp_rule_add(ctx, SCMP_ACT_ALLOW, calls[i], 0);
+    }
 }
 
-const auto BASIC_CALLS = std::vector<int>({
-    SCMP_SYS(read),
-    SCMP_SYS(write),
-    SCMP_SYS(open),
-    SCMP_SYS(close),
-    SCMP_SYS(stat),
-    SCMP_SYS(fstat),
-    SCMP_SYS(arch_prctl),
-    SCMP_SYS(lseek),
-    SCMP_SYS(mmap),
-    SCMP_SYS(mprotect),
-    SCMP_SYS(munmap),
-    SCMP_SYS(brk),
-    SCMP_SYS(pread64),
-    SCMP_SYS(writev),
-    SCMP_SYS(access),
-    SCMP_SYS(execve),
-    SCMP_SYS(uname),
-    SCMP_SYS(readlink),
-    SCMP_SYS(arch_prctl),
-    SCMP_SYS(time),
-    SCMP_SYS(futex),
-    SCMP_SYS(set_thread_area),
-    SCMP_SYS(exit_group),
-    SCMP_SYS(mq_open),
-    SCMP_SYS(unshare),
-    SCMP_SYS(set_robust_list),
-    SCMP_SYS(splice),
-    SCMP_SYS(getrandom),
-    SCMP_SYS(openat),
-    SCMP_SYS(times),
-    -1
-});
+int BASIC_CALLS[] = {SCMP_SYS(read), SCMP_SYS(fstat),
+                    SCMP_SYS(mmap), SCMP_SYS(mprotect),
+                    SCMP_SYS(munmap), SCMP_SYS(uname),
+                    SCMP_SYS(arch_prctl), SCMP_SYS(brk),
+                    SCMP_SYS(access), SCMP_SYS(exit_group),
+                    SCMP_SYS(close), SCMP_SYS(readlink),
+                    SCMP_SYS(sysinfo), SCMP_SYS(write),
+                    SCMP_SYS(writev), SCMP_SYS(lseek),
+                    SCMP_SYS(open), SCMP_SYS(openat),
+                    SCMP_SYS(clock_gettime)};
+
 
 void basicFilter(scmp_filter_ctx filter) {
     allowCalls(filter, BASIC_CALLS);
@@ -49,7 +28,6 @@ void basicFilter(scmp_filter_ctx filter) {
 
 scmp_filter_ctx generateFilter(const char* key) {
     if (strncmp(key, "none", 5) == 0) {
-        printf("Skipping seccomp filters!\n");
         return seccomp_init(SCMP_ACT_ALLOW);
     }
     scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_KILL);
@@ -61,10 +39,8 @@ scmp_filter_ctx generateFilter(const char* key) {
     seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(execve), 0);
 
     if (strncmp(key, "basic", 6)==0) {
-        printf("Applying seccomp filter basic\n");
         basicFilter(ctx);
         return ctx;
     }
-    printf("Filter not found!\n");
     return ctx;
 }
